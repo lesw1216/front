e2ef4b42ce8f5528872bcc41261a81e418c70d0b
sangwoo <lesw1216@gmail.com>
2024-03-22 20:58:10 +0900

    feat: InputComp 생성
    Input 공통 컴포넌트 생성

---
+++ src/components/InputComp.vue
@@ -0,0 +1,18 @@
+<template>
+    <input :type="type" :placeholder="placeHolder" class="p-1 border-2 focus:outline-none focus:border-blue-600"
+        :class="width"/>
+</template>
+<script setup>
+import { ref } from 'vue';
+const pops = defineProps({
+    type: String,
+    placeHolder: String,
+    width: String
+})
+
+const width = ref("w-" + pops.width)
+
+</script>
+<style lang="">
+    
+</style>
