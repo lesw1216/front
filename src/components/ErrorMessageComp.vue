645f717a5ca7443986e4cbd51c01d26cf99b67ac
sangwoo <lesw1216@gmail.com>
2024-03-27 10:16:37 +0900

    feat: ErrorMessage 컴포넌트 생성

---
+++ src/components/ErrorMessageComp.vue
@@ -0,0 +1,15 @@
+<template>
+    <div v-show="isError" class="text-red-500 text-xs font-semibold mb-2">
+        {{ message }}
+    </div>
+</template>
+<script setup>
+const props = defineProps({
+    message: String,
+    isError: Boolean,
+})
+
+</script>
+<style lang="">
+    
+</style>
